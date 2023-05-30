2b585b51671cded8adb1055cb6a94a3a360f1d96
teerasej <teerasej@gmail.com>
2023-05-30 09:41:45 +0700

    complete simple summary

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.SemanticKernel;
+
+var kernel = Kernel.Builder.Build();
+
+
+// Azure OpenAI
+// kernel.Config.AddAzureTextCompletionService(
+//     "text-davinci-003",                  // Azure OpenAI Deployment Name
+//     "https://contoso.openai.azure.com/", // Azure OpenAI Endpoint
+//     "...your Azure OpenAI Key...",       // Azure OpenAI Key
+// );
+
+// Alternative using OpenAI
+kernel.Config.AddOpenAITextCompletionService(
+    "text-davinci-003",               // OpenAI Model name
+    ""       // OpenAI API Key
+);
+
+var prompt = @"{{$input}}
+
+One line TLDR with the fewest words.";
+
+var summarize = kernel.CreateSemanticFunction(prompt);
+
+string text = @"
+1st Law of Thermodynamics - Energy cannot be created or destroyed.
+2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
+3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy.";
+
+Console.WriteLine(await summarize.InvokeAsync(text));
